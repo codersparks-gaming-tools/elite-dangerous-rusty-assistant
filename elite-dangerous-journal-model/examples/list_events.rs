b5698f981d037df1ba2a7e78a5438486edc83f72
codersparks <codersparks@users.noreply.github.com>
2025-01-12 14:18:16 +0000

    Updated list_Events to have more useful error docs

--- elite-dangerous-journal-model/examples/list_events.rs
+++ elite-dangerous-journal-model/examples/list_events.rs
@@ -2,8 +2,9 @@ use elite_dangerous_journal_model::events::fss_signal_discovered::FssSignalType;
 use elite_dangerous_journal_model::JournalEvent;
 use std::collections::HashSet;
 use std::fs::{metadata, File};
-use std::io::BufReader;
-use std::path::PathBuf;
+use std::io;
+use std::io::{BufRead, BufReader};
+use std::path::{Path, PathBuf};
 use tracing::{debug, error, info, warn};
 
 fn main() {
@@ -61,29 +62,37 @@ fn process_path(unknown_count: &mut i32, unknown_signal_types: &mut HashSet<Stri
         let f = File::open(path).expect("Could not open file");
         let reader = BufReader::new(f);
 
-        let it = serde_json::Deserializer::from_reader(reader).into_iter::<JournalEvent>();
+        let lines = reader.lines();
 
-        for (event_num, item) in it.enumerate() {
+        //let it = serde_json::Deserializer::from_reader(reader).into_iter::<JournalEvent>();
+
+        for (event_num, line) in lines.enumerate() {
+
+            let line = line.expect("Could not read line");
+
+            let item = serde_json::from_str::<JournalEvent>(&line);
             match item {
                 Ok(event) => {
-                    debug!("Event: {}, event: {:?}", event_num + 1, event);
 
-                    match event {
-                        JournalEvent::Unknown => {
-                            *unknown_count += 1;
-                            warn!("Unknown event {}", event_num + 1);
-                        }
-                        JournalEvent::FSSSignalDiscovered(event) => match event.signal_type {
-                            FssSignalType::Unknown(signal_type) => {
-                                unknown_signal_types.insert(signal_type);
+                            debug!("Event: {}, event: {:?}", event_num + 1, event);
+
+                            match event {
+                                JournalEvent::Unknown => {
+                                    *unknown_count += 1;
+                                    warn!("Unknown event {}", event_num + 1);
+                                }
+                                JournalEvent::FSSSignalDiscovered(event) => match event.signal_type {
+                                    FssSignalType::Unknown(signal_type) => {
+                                        unknown_signal_types.insert(signal_type);
+                                    }
+                                    _ => {}
+                                },
+                                _ => {}
                             }
-                            _ => {}
-                        },
-                        _ => {}
-                    }
-                }
+                        }
+
                 Err(e) => {
-                    error!("Event: {}, error: {:?}", event_num + 1, e);
+                    error!("{e} - line {line}");
                 }
             }
         }
